ec84ff22b83b2d8c9501a8fac0aa4bca94f1d39d
Carol Guirguis <Carol.Guirguis@itworx.com>
2022-09-06 10:35:54 +0200

    styling users

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,6 +3,11 @@ import { useDispatch, useSelector, connect } from "react-redux";
 import { fetchUsers } from "../store/users/users.actions";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { DataGrid } from '@mui/x-data-grid';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import {AiOutlineSearch} from 'react-icons/ai';
+
 
 
 const columns = [
@@ -11,9 +16,6 @@ const columns = [
   { field: 'lastName', headerName: 'Last name', width: 130 }
 
 ];
-
-
-
 
 export default function Users () {
   const dispatch = useDispatch();
@@ -25,7 +27,34 @@ export default function Users () {
   }, []);
   if (users.length !== 0){
     return (
-      <div style={{ height: 400, width: '100%' }}>
+  
+      <div>
+            <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > :not(style)': {
+          m: 1,
+          width: '100%',
+          height: 500,
+        },
+      }}
+    >
+      
+      <Paper elevation={3} >
+     
+      <Box
+      sx={{
+        width: 500,
+        maxWidth: '100%',
+        marginBottom:'1rem',
+        marginTop:'1rem',
+        marginLeft:'1rem'
+      }}
+    >
+      <TextField fullWidth label="Search by first name.." id="fullWidth" />
+    </Box>
+    <div style={{ height: 400, width: '100%' }}>
     <DataGrid
       rows={Object.values(users)[0]}
       columns={columns}
@@ -33,5 +62,11 @@ export default function Users () {
       rowsPerPageOptions={[10]}
       checkboxSelection
     />
-  </div>);}
+  </div>
+        </Paper>
+    </Box>
+
+     
+  </div>
+  );}
   }
